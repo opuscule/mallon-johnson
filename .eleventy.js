b4978b71083973fef50f06cbcdc0b4e8027d3ba0
Robert C. Bowman III <bowman@opuscule.com>
2023-10-30 08:17:06 -0400

    Address & Phone Updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,11 @@
 const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
-
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addPassthroughCopy('./src/js');
 	eleventyConfig.addPassthroughCopy('./src/styles');
 	eleventyConfig.addPassthroughCopy('./src/resource');
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
-	eleventyConfig.addPlugin(lazyImagesPlugin);
 	eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 	return {
 		dir: {
